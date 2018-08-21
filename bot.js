9228b37ac9052c61a673f4ec99b11751aa8b0a65
hamossss <40631448+hamossss@users.noreply.github.com>
2018-08-21 13:09:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2919,7 +2919,7 @@ client.on("guildMemberAdd", member => {
       var Canvas = require('canvas')
       var jimp = require('jimp')
       
-      const w = ['PicsArt_07-17-07.58.02 (1).png'];
+      const w = ['S7Q (2).png'];
       
               let Image = Canvas.Image,
                   canvas = new Canvas(557, 241),
