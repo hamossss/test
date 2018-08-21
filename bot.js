ceb9b23ff0666d12db633f5a1ed1c835b36e5eb2
hamossss <40631448+hamossss@users.noreply.github.com>
2018-08-21 13:12:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2949,7 +2949,7 @@ client.on("guildMemberAdd", member => {
                               ctx.font = '30px Arial';
                               ctx.fontSize = '28px';
                               ctx.fillStyle = "#FFFFFF";
-      ctx.fillText(`Welcome To ${member.guild.name}`, 245, 80);
+      ctx.fillText(``, 245, 80);
       
                               //AVATARً
                               let Avatar = Canvas.Image;
